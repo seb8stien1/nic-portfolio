62dbc691811be01552fe16d36bc8c0b95706538b
Seb <sebastien.cantin@mail.mcgill.ca>
2023-08-09 23:43:17 -0400

    fix cuba pics

--- src/pages/CubaPage.js
+++ src/pages/CubaPage.js
@@ -7,14 +7,14 @@ import ImageDisplay from "../components/ImageDisplay";
 const photos = [
     {src:require("../resources/cuba/cuba1.jpg"), width: 1000, height: 661},
     {src:require("../resources/cuba/cuba2.jpg"), width: 1000, height: 669},
-    {src:require("../resources/cuba/cuba3.JPG"), width: 1000, height: 664},
+    {src:require("../resources/cuba/cuba3.jpg"), width: 1000, height: 664},
     {src:require("../resources/cuba/cuba4.jpg"), width: 1000, height: 667},
     {src:require("../resources/cuba/cuba5.jpg"), width: 1000, height: 694},
-    {src:require("../resources/cuba/cuba6.JPG"), width: 1000, height: 661},
-    {src:require("../resources/cuba/cuba7.JPG"), width: 1000, height: 660},
+    {src:require("../resources/cuba/cuba6.jpg"), width: 1000, height: 661},
+    {src:require("../resources/cuba/cuba7.jpg"), width: 1000, height: 660},
     {src:require("../resources/cuba/cuba8.jpg"), width: 1000, height: 664},
     {src:require("../resources/cuba/cuba9.jpg"), width: 1000, height: 661},
-    {src:require("../resources/cuba/cuba10.JPG"), width: 666, height: 1000},
+    {src:require("../resources/cuba/cuba10.jpg"), width: 666, height: 1000},
     {src:require("../resources/cuba/cuba11.jpg"), width: 1000, height: 666},
     {src:require("../resources/cuba/cuba12.jpg"), width: 1000, height: 682},
 
